60ce369b8e01751e868fc145569b5aaa31d2f417
cfrank3N <adamcecarfrank@gmail.com>
2024-10-29 14:09:55 +0100

    Added actionlistener to all buttons, and reformatted the creation of buttons

--- src/Graphic.java
+++ src/Graphic.java
@@ -1,12 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 // är adam med?
+//Adam är med!
 
-public class Graphic extends JFrame {
+public class Graphic extends JFrame implements ActionListener {
     JPanel panel = new JPanel();
     List<JButton> buttons = new ArrayList<>();
 
@@ -17,6 +20,8 @@ public class Graphic extends JFrame {
         gameLayout();
         for (JButton b : buttons) {
             panel.add(b);
+            b.addActionListener(this);
+            b.setOpaque(true);
         }
         setVisible(true);
         pack();
@@ -26,13 +31,13 @@ public class Graphic extends JFrame {
     //Lägger till 15 knappar
     public void gameLayout() {
         for (int i = 1; i <= 16; i++) {
+            JButton b;
             if (i == 16) {
-                JButton b = (new JButton(""));
-                buttons.add(b);
+                b = (new JButton(""));
             } else {
-                JButton b = (new JButton("" + i));
-                buttons.add(b);
+                b = (new JButton("" + i));
             }
+            buttons.add(b);
         }
 
         Collections.shuffle(buttons);
@@ -43,5 +48,12 @@ public class Graphic extends JFrame {
         return "Graphic{" +
                 "buttons=" + buttons +
                 '}';
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == buttons.getFirst()) {
+            buttons.getFirst().setBackground(Color.GREEN);
+        }
     }
 }
