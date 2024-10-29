14e56ff37eb0122ed41452f5a18beefadeda1e80
Gaurgle <justwannagroove@gmail.com>
2024-10-29 09:11:10 +0100

    initial commit

--- src/Graphic.java
+++ src/Graphic.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Graphic {
+public class Graphic extends JFrame {
+        JPanel panel = new JPanel();
+        List<JButton> buttons = new ArrayList<>();
+
+        public void Graphic(){
+            setTitle("Graphic");
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setSize(800, 600);
+
+            gameLayout();
+
+            add(panel);
+
+        }
+
+    public void gameLayout() {
+        panel.setLayout(new GridLayout(3, 5,10,10));
+
+        for (int i = 1; i <= 15; i++) {
+            JButton b = (new JButton("b" +i));
+            buttons.add(b);
+            panel.add(b);
+        }
+
+        for (JButton button : buttons){
+            System.out.println(button.getText());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Graphic{" +
+                "buttons=" + buttons +
+                '}';
+    }
 }
