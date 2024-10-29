4b5b99ecc3709b5e3b640de8544dd0d6bad5fb32
cfrank3N <adamcecarfrank@gmail.com>
2024-10-29 17:20:02 +0100

    Got a working paint and repaint. And a working actionPerformed

--- src/Graphic.java
+++ src/Graphic.java
@@ -11,26 +11,25 @@ import java.util.List;
 
 public class Graphic extends JFrame implements ActionListener {
     JPanel panel = new JPanel();
-    List<JButton> buttons = new ArrayList<>();
+    List<JButton> buttons;
     int indexOfEmptyButton;
 
     public Graphic() {
         setTitle("Graphic");
         add(panel);
         panel.setLayout(new GridLayout(4, 4));
-        gameLayout();
-        for (JButton b : buttons) {
-            panel.add(b);
-            b.addActionListener(this);
-            b.setOpaque(true);
-        }
+        buttons = gameLayout();
+        paint();
         setVisible(true);
         pack();
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     //Lägger till 15 knappar
-    public void gameLayout() {
+    public List<JButton> gameLayout() {
+
+        List <JButton> buttons = new ArrayList<>();
+
         for (int i = 1; i <= 16; i++) {
             JButton b;
             if (i == 16) {
@@ -39,16 +38,37 @@ public class Graphic extends JFrame implements ActionListener {
                 b = (new JButton("" + i));
             }
             buttons.add(b);
+            b.addActionListener(this);
         }
 
         Collections.shuffle(buttons);
+        return buttons;
     }
+
+    public void paint() {
+        for (JButton b : buttons) {
+            panel.add(b);
+        }
+    }
+
+    public void repaint() {
+        panel.removeAll();
+        panel.revalidate();
+        paint();
+    }
+
 
     @Override
     public String toString() {
         return "Graphic{" +
                 "buttons=" + buttons +
                 '}';
+    }
+
+    public boolean isOnSameRow(int indexOne, int indexTwo) {
+        int sum = indexOne + indexTwo;
+
+        return sum > 0 && sum <= 5 || sum > 8 && sum <= 13 || sum > 16 && sum <= 21 || sum > 24 && sum <= 29;
     }
 
     @Override
@@ -61,9 +81,21 @@ public class Graphic extends JFrame implements ActionListener {
             }
         }
 
-        if (i - 1 == indexOfEmptyButton || i + 1 == indexOfEmptyButton || i + 4 == indexOfEmptyButton || i -4 == indexOfEmptyButton) {
+        System.out.println(i);
+        System.out.println(indexOfEmptyButton);
+
+        //Finslipa denna. Just nu byter den om den ligger en rad ovanför men är ej direkt bredvid eller ovanför knappen man tryckte.
+        //Revalidate och Repaint är våra vänner.
+        //Förenkla nedan statement.
+        if (i + 4 == indexOfEmptyButton || i -4 == indexOfEmptyButton) {
             //Byt plats på den tomma knappen och den tryckta knappen
             System.out.println("Change places!");
+            Collections.swap(buttons, i, indexOfEmptyButton);
+            repaint();
+        } else if (isOnSameRow(i, indexOfEmptyButton) && i - 1 == indexOfEmptyButton || isOnSameRow(i, indexOfEmptyButton) && i + 1 == indexOfEmptyButton) {
+            System.out.println("Change places!");
+            Collections.swap(buttons, i, indexOfEmptyButton);
+            repaint();
         }
 
 
