2025fece7b4e77ca5a0cb030a38582ce7c81a69f
cfrank3N <adamcecarfrank@gmail.com>
2024-10-30 20:36:32 +0100

    Added button styling for buttons in OptionPanel as well

--- src/OptionsPanel.java
+++ src/OptionsPanel.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.CompoundBorder;
 import javax.swing.border.MatteBorder;
 import java.awt.*;
 
@@ -12,14 +13,16 @@ public class OptionsPanel extends JPanel {
         restartGame.setFont(new Font("Century Gothic", Font.BOLD, 15));
         restartGame.setBackground(new Color(205, 171, 255));
         restartGame.setForeground(new Color(222, 200, 255));
-        restartGame.setBorder(new MatteBorder(2, 5, 8, 5, new Color(222, 200, 255)));
+        restartGame.setBorder(new CompoundBorder(new MatteBorder(2, 6, 11, 6, new Color(164, 118, 255)),
+                new MatteBorder(0, 2, 5, 2, new Color(176, 129, 255))));
         setBackground(Color.WHITE);
         add(restartGame);
 
         setGameOneFromWin.setFont(new Font("Century Gothic", Font.BOLD, 15));
         setGameOneFromWin.setBackground(new Color(205, 171, 255));
         setGameOneFromWin.setForeground(new Color(222, 200, 255));
-        setGameOneFromWin.setBorder(new MatteBorder(2, 5, 8, 5, new Color(222, 200, 255)));
+        setGameOneFromWin.setBorder(new CompoundBorder(new MatteBorder(2, 6, 11, 6, new Color(164, 118, 255)),
+                new MatteBorder(0, 2, 5, 2, new Color(176, 129, 255))));
         add(setGameOneFromWin);
     }
 
